b7cb0cd9d56369f4f5b023dffa1836fb39d4c16b
Tyler <tylhuang@umich.edu>
2022-09-15 14:01:43 -0400

    added insta485 files

---
+++ insta485/config.py
@@ -0,0 +1,14 @@
+"""Insta485 development configuration."""
+import pathlib
+# Root of this application, useful if it doesn't occupy an entire domain
+APPLICATION_ROOT = '/'
+# Secret key for encrypting cookies
+SECRET_KEY = b'\xaf\xed\xaaR\x18:\\\xbd\x16\xc9\xfb\x0c.\xf8\xa5:\xfb\xfc\x84*\x1c\xbd3\xdd'
+SESSION_COOKIE_NAME = 'login'
+# File Upload to var/uploads/
+INSTA485_ROOT = pathlib.Path(__file__).resolve().parent.parent
+UPLOAD_FOLDER = INSTA485_ROOT/'var'/'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+# Database file is var/insta485.sqlite3
+DATABASE_FILENAME = INSTA485_ROOT/'var'/'insta485.sqlite3'
