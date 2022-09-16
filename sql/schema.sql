9a65843e3e014627aa8beb99bcd656454d6d5fe6
Tyler <tylhuang@umich.edu>
2022-09-16 18:03:42 -0400

    fixing scripts + added flask

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
   email VARCHAR(40) NOT NULL,
   filename VARCHAR(64) NOT NULL, 
   password VARCHAR(256) NOT NULL,
-  created DATETIME DEFAULT(getdate()), 
+  created DATETIME DEFAULT CURRENT_TIMESTAMP, 
   PRIMARY KEY(username)
 );
 
@@ -14,7 +14,7 @@ CREATE TABLE posts(
   postid INTEGER AUTO_INCREMENT,
   filename VARCHAR(64) NOT NULL,
   owner VARCHAR(20),
-  created DATETIME DEFAULT(getdate()), 
+  created DATETIME DEFAULT CURRENT_TIMESTAMP, 
   PRIMARY KEY(postid),
   FOREIGN KEY(owner) REFERENCES users
     ON DELETE CASCADE 
@@ -23,7 +23,7 @@ CREATE TABLE posts(
 CREATE TABLE following(
   username1 VARCHAR(20),
   username2 VARCHAR(20),
-  created DATETIME DEFAULT(getdate()), 
+  created DATETIME DEFAULT CURRENT_TIMESTAMP, 
   PRIMARY KEY(username1, username2)
   FOREIGN KEY(username1) REFERENCES users
     ON DELETE CASCADE,
@@ -36,7 +36,7 @@ CREATE TABLE comments(
   owner VARCHAR(20), 
   postid INTEGER, 
   text VARCHAR(1024),
-  created DATETIME DEFAULT(getdate()),
+  created DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(commentid),
   FOREIGN KEY(owner) REFERENCES users
     ON DELETE CASCADE,
@@ -48,7 +48,7 @@ CREATE TABle likes(
   likeid INTEGER AUTO_INCREMENT,
   owner VARCHAR(20),
   postid INTEGER,
-  created DATETIME DEFAULT(getdate()),
+  created DATETIME DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY(owner) REFERENCES users
     ON DELETE CASCADE,
   FOREIGN KEY(postid) REFERENCES posts
